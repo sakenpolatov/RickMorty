f96f746fb3f345da397106ded48635cf8055093a
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-03-16 12:30:59 +0500

    feat: добавил пути для компонентов (Routes, Route)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import './App.css'
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { Home } from './components/Home/Home.jsx'
+import { Categories } from './components/Categories/Categories.jsx'
+import { Details } from './components/Details/Details.jsx'
+import { NotFound } from './components/NotFound/NotFound.jsx'
+import styles from './app.module.css'
 
 function App() {
-	return <div className='App'></div>
+	return (
+		<div className={styles.app}>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/:categories' element={<Categories />} />
+				<Route path='/:categories/:id' element={<Details />} />
+				<Route path='*' element={<NotFound />} />
+			</Routes>
+		</div>
+	)
 }
 
 export default App
