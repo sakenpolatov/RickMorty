e2713fb285cb0eb08d70b48a59f8ba5c634af120
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-03-20 00:17:08 +0500

    feat : разметил навигацию

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import icon from '../../assets/rick.svg'
+import styles from './header.module.css'
+
+export function Header() {
+	return (
+		<header>
+			<div className={styles.logo}>
+				<Link to='/'>
+					<img src={icon} alt='Icon' width={100} height={100} />
+				</Link>
+			</div>
+			<div className={styles.nav}>
+				<ul>
+					<li>
+						<Link to='/characters'>Characters</Link>
+					</li>
+					<li>
+						<Link to='/locations'>Locations</Link>
+					</li>
+					<li>
+						<Link to='/episodes'>Episodes</Link>
+					</li>
+				</ul>
+			</div>
+		</header>
+	)
+}
