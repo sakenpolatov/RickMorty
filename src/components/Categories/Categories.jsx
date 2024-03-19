40d6aae686d528437ade7f15a40f3f8fe18efa26
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-03-20 00:20:56 +0500

    feat : рендер списка элементов(герои,локации,эпизоды)

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,5 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams, Link } from 'react-router-dom'
+import charactersData from '../../assets/data/characters.json'
+import locationData from '../../assets/data/locations.json'
+import episodeData from '../../assets/data/episodes.json'
 
 export function Categories() {
-	return <div>categories</div>
+	const { category } = useParams()
+	const [data, setData] = useState([])
+	const [isLoading, setIsLoading] = useState(true)
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				setIsLoading(true)
+				let categoryData = []
+
+				if (category === 'characters') {
+					categoryData = charactersData
+				} else if (category === 'locations') {
+					categoryData = locationData
+				} else if (category === 'episodes') {
+					categoryData = episodeData
+				}
+
+				setData(categoryData)
+				setIsLoading(false)
+			} catch (error) {
+				console.error('Ошибка получения данных:', error)
+				setIsLoading(false)
+			}
+		}
+
+		fetchData()
+	}, [category])
+
+	return (
+		<div>
+			{isLoading ? (
+				<p>Loading...</p>
+			) : (
+				<ul>
+					{data.map(item => (
+						<li key={item.id}>
+							<Link to={`/${category}/${item.id}`}>{item.name}</Link>
+						</li>
+					))}
+				</ul>
+			)}
+		</div>
+	)
 }
