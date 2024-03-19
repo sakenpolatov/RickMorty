987350273b00cc2eae7acafce54541b7b73c4009
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-03-20 00:15:38 +0500

    feat : сократил app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import { Home } from './components/Home/Home.jsx'
-import { Categories } from './components/Categories/Categories.jsx'
-import { Details } from './components/Details/Details.jsx'
-import { NotFound } from './components/NotFound/NotFound.jsx'
+import { Header } from './components/Header/Header.jsx'
+import { Router } from './components/Router/Router.jsx'
 import styles from './app.module.css'
 
 function App() {
 	return (
 		<div className={styles.app}>
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/:categories' element={<Categories />} />
-				<Route path='/:categories/:id' element={<Details />} />
-				<Route path='*' element={<NotFound />} />
-			</Routes>
+			<Header />
+			<Router />
 		</div>
 	)
 }
