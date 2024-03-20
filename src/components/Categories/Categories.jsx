93089992c2866f6344a13f55a46ed5548ccdbcf5
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-03-20 13:46:43 +0500

    fix : поправил разметку

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import styles from './categories.module.css'
 import { useParams, Link } from 'react-router-dom'
 import charactersData from '../../assets/data/characters.json'
 import locationData from '../../assets/data/locations.json'
@@ -39,13 +40,15 @@ export function Categories() {
 			{isLoading ? (
 				<p>Loading...</p>
 			) : (
-				<ul>
-					{data.map(item => (
-						<li key={item.id}>
-							<Link to={`/${category}/${item.id}`}>{item.name}</Link>
-						</li>
-					))}
-				</ul>
+				<div className={styles.list}>
+					<ul className={styles.categories}>
+						{data.map(item => (
+							<li key={item.id}>
+								<Link to={`/${category}/${item.id}`}>{item.name}</Link>
+							</li>
+						))}
+					</ul>
+				</div>
 			)}
 		</div>
 	)
