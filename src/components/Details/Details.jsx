59522ac52ca894939b972c7f069e5049d5df0c47
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-03-20 16:21:18 +0500

    feat : добавил запрос данных через fetch

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,25 +1,59 @@
-import React, { useState, useEffect } from 'react'
 import styles from './details.module.css'
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import charactersData from '../../assets/data/characters.json'
-import episodesData from '../../assets/data/episodes.json'
-import locationsData from '../../assets/data/locations.json'
+import { Loading } from '../Loading/Loading'
 
 export function Details() {
 	const { category, id } = useParams()
 	const [details, setDetails] = useState(null)
+	const [isLoading, setIsLoading] = useState(true)
 
 	useEffect(() => {
-		const dataMap = {
-			characters: charactersData,
-			episodes: episodesData,
-			locations: locationsData
-		}
+		const fetchData = async () => {
+			setIsLoading(true)
 
-		const fetchData = () => {
-			const data =
-				dataMap[category].find(item => item.id === parseInt(id)) || null
-			setDetails(data)
+			try {
+				const response = await fetch(
+					`https://65faa45d3909a9a65b1affc6.mockapi.io/rickandmorty/data`
+				)
+
+				if (!response.ok) {
+					throw new Error('Ошибка получения данных')
+				}
+
+				const jsonData = await response.json()
+
+				if (!Array.isArray(jsonData) || jsonData.length !== 3) {
+					throw new Error('Неверная структура данных')
+				}
+
+				let categoryData = null
+
+				if (category === 'characters') {
+					categoryData = jsonData[0]
+				} else if (category === 'locations') {
+					categoryData = jsonData[1]
+				} else if (category === 'episodes') {
+					categoryData = jsonData[2]
+				}
+
+				if (categoryData) {
+					const item = categoryData.find(item => item.id === parseInt(id))
+					if (item) {
+						setDetails(item)
+					} else {
+						throw new Error(
+							`Элемент с таким id ${id} не найден в категории ${category} в базе`
+						)
+					}
+				} else {
+					throw new Error(`Данные в категории ${category} не найдены в базе`)
+				}
+			} catch (error) {
+				console.error('Ошибка получения данных:', error)
+			} finally {
+				setIsLoading(false)
+			}
 		}
 
 		fetchData()
@@ -27,63 +61,69 @@ export function Details() {
 
 	return (
 		<div className={styles.details}>
-			{details ? (
+			{isLoading ? (
+				<Loading />
+			) : (
 				<div className={styles.content}>
-					<h2>{details.name}</h2>
-					{category === 'characters' && (
-						<div className={styles.characters}>
-							<div>
-								<img
-									className={styles.img}
-									src={details.image}
-									alt={details.name}
-								/>
-							</div>
-							<div className={styles.info}>
-								<p>
-									Status: <u>{details.status}</u>
-								</p>
-								<p>
-									Species: <u>{details.species}</u>
-								</p>
-								<p>
-									Gender: <u>{details.gender}</u>
-								</p>
-								<p>
-									Created date: <u>{details.created}</u>
-								</p>
-							</div>
-						</div>
-					)}
-					{category === 'episodes' && (
-						<div className={styles.episodes}>
-							<p>
-								Air Date:<u> {details.air_date}</u>
-							</p>
-							<p>
-								Season-Episode: <u>{details.episode}</u>
-							</p>
-							<p>
-								Created date: <u>{details.created}</u>
-							</p>
-						</div>
-					)}
-					{category === 'locations' && (
-						<div className={styles.locations}>
-							<p>
-								Type: <u>{details.type}</u>
-							</p>
-							<p>
-								Dimension: <u>{details.dimension}</u>
-							</p>
-							<p>
-								Created date: <u>{details.created}</u>
-							</p>
-						</div>
+					{details ? (
+						<>
+							<h2>{details.name}</h2>
+							{category === 'characters' && (
+								<div className={styles.characters}>
+									<div>
+										<img
+											className={styles.img}
+											src={details.image}
+											alt={details.name}
+										/>
+									</div>
+									<div className={styles.info}>
+										<p>
+											Status: <u>{details.status}</u>
+										</p>
+										<p>
+											Species: <u>{details.species}</u>
+										</p>
+										<p>
+											Gender: <u>{details.gender}</u>
+										</p>
+										<p>
+											Created date: <u>{details.created}</u>
+										</p>
+									</div>
+								</div>
+							)}
+							{category === 'episodes' && (
+								<div className={styles.episodes}>
+									<p>
+										Air Date:<u> {details.air_date}</u>
+									</p>
+									<p>
+										Season-Episode: <u>{details.episode}</u>
+									</p>
+									<p>
+										Created date: <u>{details.created}</u>
+									</p>
+								</div>
+							)}
+							{category === 'locations' && (
+								<div className={styles.locations}>
+									<p>
+										Type: <u>{details.type}</u>
+									</p>
+									<p>
+										Dimension: <u>{details.dimension}</u>
+									</p>
+									<p>
+										Created date: <u>{details.created}</u>
+									</p>
+								</div>
+							)}
+						</>
+					) : (
+						<p>Item not found.</p>
 					)}
 				</div>
-			) : (
-				<p>Loading...</p>
 			)}
 		</div>
 	)
