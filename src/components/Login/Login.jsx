29adfc8111bfc3259b0f3947e8c5c70f65494187
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-03-26 04:04:04 +0500

    feat : добавлен компонент авторизации

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,36 @@
 import React from 'react'
+import styles from './login.module.css'
+import { useNavigate, useLocation } from 'react-router-dom'
+import { useAuth } from '../../context/AuthProvider/AuthProvider'
 
 export function Login() {
-	return <div></div>
+	const navigate = useNavigate()
+	const location = useLocation()
+	const auth = useAuth()
+
+	const from = location.state?.from || '/'
+
+	const handleSubmit = event => {
+		event.preventDefault()
+		const formData = new FormData(event.currentTarget)
+		const userName = formData.get('username')
+
+		auth.signIn(userName, () => {
+			navigate(from, { replace: true })
+		})
+	}
+
+	return (
+		<div className={styles.loginContainer}>
+			<form className={styles.loginForm} onSubmit={handleSubmit}>
+				<label className={styles.loginLabel}>
+					Username:{'            '}
+					<input className={styles.loginInput} type='text' name='username' />
+				</label>
+				<button className={styles.loginButton} type='submit'>
+					Log In
+				</button>
+			</form>
+		</div>
+	)
 }
