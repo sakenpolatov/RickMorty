b28f09fc205ed346dfa94b5b301612135952ed70
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-03-26 04:03:32 +0500

    feat : добавлен приватный роутинг на страницы Categories & Details

--- src/components/Router/Router.jsx
+++ src/components/Router/Router.jsx
@@ -1,20 +1,34 @@
-// Router.js
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { Home } from '../Home/Home'
 import { Categories } from '../Categories/Categories'
 import { Details } from '../Details/Details'
 import { NotFound } from '../NotFound/NotFound'
+import { Login } from '../Login/Login'
+import { PrivateRoute } from '../PrivateRoute/PrivateRoute'
 
 export function Router() {
 	return (
-		<div>
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/categories/:category' element={<Categories />} />
-				<Route path='/categories/:category/:id' element={<Details />} />
-				<Route path='*' element={<NotFound />} />
-			</Routes>
-		</div>
+		<Routes>
+			<Route path='/' element={<Home />} />
+			<Route
+				path='/categories/:category'
+				element={
+					<PrivateRoute>
+						<Categories />
+					</PrivateRoute>
+				}
+			/>
+			<Route
+				path='/categories/:category/:id'
+				element={
+					<PrivateRoute>
+						<Details />
+					</PrivateRoute>
+				}
+			/>
+			<Route path='/login' element={<Login />} />
+			<Route path='*' element={<NotFound />} />
+		</Routes>
 	)
 }
