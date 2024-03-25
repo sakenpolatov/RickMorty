4c6e50351a3e69ec836e8d6b9154b3bf13fbd069
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-03-26 04:01:00 +0500

    feat : добавлен Authprovider

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react'
 import { Header } from './components/Header/Header.jsx'
 import { Router } from './components/Router/Router.jsx'
 import styles from './app.module.css'
+import { AuthProvider } from './context/AuthProvider/AuthProvider.jsx'
 
 function App() {
 	return (
 		<div className={styles.app}>
-			<Header />
-			<Router />
+			<AuthProvider>
+				<Header />
+				<Router />
+			</AuthProvider>
 		</div>
 	)
 }
