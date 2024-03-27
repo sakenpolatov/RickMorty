5766c1af3f27949c523764ab53b5d34c887b5720
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-03-28 02:24:49 +0500

    feat : add lazy() динамические импорты

--- src/components/Router/Router.jsx
+++ src/components/Router/Router.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { lazy } from 'react'
 import { Routes, Route } from 'react-router-dom'
-import { Home } from '../pages/Home/Home'
-import { Categories } from '../pages/Categories/Categories'
-import { Details } from '../pages/Details/Details'
-import { NotFound } from '../pages/NotFound/NotFound'
-import { Login } from '../Login/Login'
 import { PrivateRoute } from '../PrivateRoute/PrivateRoute'
+
+const Home = lazy(() =>
+	import('../pages/Home/Home').then(module => ({
+		default: module.Home
+	}))
+)
+
+const Categories = lazy(() =>
+	import('../pages/Categories/Categories').then(module => ({
+		default: module.Categories
+	}))
+)
+const Details = lazy(() =>
+	import('../pages/Details/Details').then(module => ({
+		default: module.Details
+	}))
+)
+const Login = lazy(() =>
+	import('../Login/Login').then(module => ({
+		default: module.Login
+	}))
+)
+const NotFound = lazy(() =>
+	import('../pages/NotFound/NotFound').then(module => ({
+		default: module.NotFound
+	}))
+)
 
 export function Router() {
 	return (
