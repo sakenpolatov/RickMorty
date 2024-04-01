ea9ef3990614865dcd0bf2889b2b0fb6d47ddb44
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-04-01 22:14:27 +0500

    temp : add temp files

--- src/components/pages/Categories/Categories.jsx
+++ src/components/pages/Categories/Categories.jsx
@@ -1,66 +1,60 @@
-import React, { useState, useRef, useCallback, useEffect } from 'react'
-import styles from './categories.module.css'
-import { useParams, Link, useNavigate } from 'react-router-dom'
-import { Loading } from '../../Loading/Loading'
-import { useGetData } from '../../../hooks/useGetData'
+import React, { useState, useRef, useCallback, useEffect } from "react";
+import styles from "./categories.module.css";
+import { useParams, Link, useNavigate } from "react-router-dom";
+import { Loading } from "../../Loading/Loading";
+import { useGetData } from "../../../hooks/useGetData";
 
 export function Categories() {
-	const navigate = useNavigate()
-	const { category } = useParams()
-	const [pageNumber, setPageNumber] = useState(1)
-	const { isLoading, data, hasMore, isPending } = useGetData(pageNumber)
+  const navigate = useNavigate();
+  const { category } = useParams();
+  const [pageNumber, setPageNumber] = useState(1);
+  const { isLoading, data, hasMore, isPending } = useGetData(pageNumber);
 
-	const observer = useRef()
-	const lastNodeRef = useCallback(
-		node => {
-			if (isLoading || isPending) return // Проверяем, что нет процесса загрузки
-			if (observer.current) {
-				observer.current.disconnect()
-			}
-			observer.current = new IntersectionObserver(entries => {
-				console.log('VISIBLE')
-				if (entries[0].isIntersecting && hasMore) {
-					setPageNumber(prevState => prevState + 1)
-				}
-			})
-			if (node) {
-				observer.current.observe(node)
-			}
-			console.log('###node###', node)
-		},
-		[isLoading, isPending, hasMore]
-	)
+  const observer = useRef();
+  const lastNodeRef = useCallback(
+    (node) => {
+      if (isLoading || isPending) return;
+      if (observer.current) {
+        observer.current.disconnect();
+      }
+      observer.current = new IntersectionObserver((entries) => {
+        console.log("VISIBLE");
+        if (entries[0].isIntersecting && hasMore) {
+          setPageNumber((prevState) => prevState + 1);
+        }
+      });
+      if (node) {
+        observer.current.observe(node);
+      }
+      console.log("###node###", node);
+    },
+    [isLoading, isPending, hasMore]
+  );
 
-	useEffect(() => {
-		if (lastNodeRef.current) {
-			lastNodeRef.current.scrollIntoView({ behavior: 'smooth', block: 'end' }) // Прокрутка к последнему элементу
-		}
-	}, [data])
+  if (!["character", "episode", "location"].includes(category)) {
+    navigate("/notfound");
+  }
 
-	if (!['character', 'episode', 'location'].includes(category)) {
-		navigate('/notfound')
-	}
-
-	return (
-		<div>
-			{isPending || isLoading ? (
-				<Loading />
-			) : (
-				<div className={styles.list}>
-					<div>
-						<ul className={styles.categories}>
-							{data.map((item, index) => (
-								<li key={item.id}>
-									<Link to={`/categories/${category}/${item.id}`}>
-										{item.name}
-									</Link>
-									{index === data.length - 1 ? <div ref={lastNodeRef} /> : null}
-								</li>
-							))}
-						</ul>
-					</div>
-				</div>
-			)}
-		</div>
-	)
+  return (
+    <div>
+      {isPending || isLoading ? (
+        <Loading />
+      ) : (
+        <div className={styles.list}>
+          <div>
+            <ul className={styles.categories}>
+              {data.map((item, index) => (
+                <li key={item.id}>
+                  <Link to={`/categories/${category}/${item.id}`}>
+                    {item.name}
+                  </Link>
+                  {index === data.length - 1 ? <div ref={lastNodeRef} /> : null}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
--- src/hooks/useGetData.js
+++ src/hooks/useGetData.js
@@ -1,52 +1,54 @@
-import { useEffect, useState, useTransition, useMemo } from 'react'
-import axios from 'axios'
-import { useParams } from 'react-router-dom'
+import { useEffect, useState, useTransition, useMemo } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 export function useGetData(pageNumber) {
-	const [isLoading, setIsLoading] = useState(true)
-	const [data, setData] = useState([])
-	const [hasMore, setHasMore] = useState(false)
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [hasMore, setHasMore] = useState(false);
 
-	const [isPending, startTransition] = useTransition()
-	const { category } = useParams()
+  const [isPending, startTransition] = useTransition();
+  const { category } = useParams();
 
-	const getDataParams = useMemo(() => ({ category, pageNumber }), [
-		category,
-		pageNumber
-	])
+  const getDataParams = useMemo(
+    () => ({ category, pageNumber }),
+    [category, pageNumber]
+  );
 
-	useEffect(() => {
-		startTransition(() => {
-			setIsLoading(true)
-			axios({
-				method: 'GET',
-				url: `https://rickandmortyapi.com/api/${getDataParams.category}`,
-				params: {
-					page: getDataParams.pageNumber
-				}
-			})
-				.then(res => {
-					let categoryData = []
-					if (
-						getDataParams.category === 'character' ||
-						getDataParams.category === 'location' ||
-						getDataParams.category === 'episode'
-					) {
-						categoryData = res.data.results
-					} else {
-						categoryData = res.data
-					}
-					setData(prevData => [...prevData, ...categoryData])
-					setHasMore(categoryData.length > 0)
-					setIsLoading(false)
-				})
-				.catch(error => {
-					console.error('Error:', error.message)
-				})
-		})
-		console.log('###category###;', getDataParams.category)
-		console.log('###pageNumber###;', getDataParams.pageNumber)
-	}, [getDataParams])
+  useEffect(() => {
+    startTransition(() => {
+      setIsLoading(true);
+      axios({
+        method: "GET",
+        url: `https://rickandmortyapi.com/api/${getDataParams.category}`,
+        params: {
+          page: getDataParams.pageNumber,
+        },
+      })
+        .then((res) => {
+          let categoryData = [];
+          if (
+            getDataParams.category === "character" ||
+            getDataParams.category === "location" ||
+            getDataParams.category === "episode"
+          ) {
+            categoryData = res.data.results;
+          } else {
+            categoryData = res.data;
+          }
+          setData((prevData) => {
+            return [...new Set([...prevData, ...categoryData])];
+          });
+          setHasMore(categoryData.length > 0);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          console.error("Error:", error.message);
+        });
+    });
+    console.log("###category###;", getDataParams.category);
+    console.log("###pageNumber###;", getDataParams.pageNumber);
+  }, [getDataParams]);
 
-	return { isLoading, data, hasMore, isPending }
+  return { isLoading, data, hasMore, isPending };
 }
