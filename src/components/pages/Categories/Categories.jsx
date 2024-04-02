5b85acbff988eea5cd5163209f142e21d8c4a78e
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-04-02 14:01:46 +0500

    feat : вернул компонент Sort

--- src/components/pages/Categories/Categories.jsx
+++ src/components/pages/Categories/Categories.jsx
@@ -1,65 +1,72 @@
-import React, { useState, useRef, useCallback, useEffect } from "react";
-import styles from "./categories.module.css";
-import { useParams, Link, useNavigate } from "react-router-dom";
-import { Loading } from "../../Loading/Loading";
-import { useGetData } from "../../../hooks/useGetData";
+import React, { useState, useRef, useCallback, useEffect } from 'react'
+import styles from './categories.module.css'
+import { useParams, Link, useNavigate } from 'react-router-dom'
+import { Loading } from '../../Loading/Loading'
+import { useGetData } from '../../../hooks/useGetData'
+import { Sort } from '../../Sort/Sort'
+
+const initialSortType = 'default'
 
 export function Categories() {
-  const navigate = useNavigate();
-  const { category } = useParams();
-  const [pageNumber, setPageNumber] = useState(1);
-  const { isLoading, data, hasMore, isPending } = useGetData(pageNumber);
+	const navigate = useNavigate()
+	const { category } = useParams()
+	const [pageNumber, setPageNumber] = useState(1)
+	const { isLoading, data, hasMore, isPending } = useGetData(pageNumber)
+	const [sortType, setSortType] = useState(initialSortType)
 
-  const observer = useRef();
-  const lastNodeRef = useCallback(
-    (node) => {
-      if (isLoading) return;
-      if (observer.current) {
-        observer.current.disconnect();
-      }
-      observer.current = new IntersectionObserver((entries) => {
-        if (entries[0].isIntersecting && hasMore) {
-          console.log("VISIBLE");
-          setPageNumber((prevState) => prevState + 1);
-        }
-      });
-      if (node) {
-        observer.current.observe(node);
-      }
-      console.log("###node###", node);
-    },
-    [isLoading, hasMore]
-  );
+	const observer = useRef()
+	const lastNodeRef = useCallback(
+		node => {
+			if (isLoading) return
+			if (observer.current) {
+				observer.current.disconnect()
+			}
+			observer.current = new IntersectionObserver(entries => {
+				if (entries[0].isIntersecting && hasMore) {
+					console.log('VISIBLE')
+					setPageNumber(prevState => prevState + 1)
+				}
+			})
+			if (node) {
+				observer.current.observe(node)
+			}
+			console.log('###node###', node)
+		},
+		[isLoading, hasMore]
+	)
 
-  if (!["character", "episode", "location"].includes(category)) {
-    navigate("/notfound");
-  }
+	if (!['character', 'episode', 'location'].includes(category)) {
+		navigate('/notfound')
+	}
 
-  return (
-    <div>
-      <div className={styles.list}>
-        {!data.length && (isPending || isLoading) ? (
-          <Loading />
-        ) : (
-          <ul className={styles.categories}>
-            {data.map((item, index) =>
-              data.length - 15 === index + 1 ? (
-                <li ref={lastNodeRef} key={`${category}-${item.id}`}>
-                  <Link to={`/categories/${category}/${item.id}`}>
-                    {item.name}
-                  </Link>
-                </li>
-              ) : (
-                <li key={`${category}-${item.id}`}>
-                  <Link to={`/categories/${category}/${item.id}`}>
-                    {item.name}
-                  </Link>
-                </li>
-              )
-            )}
-          </ul>
-        )}
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className={styles.list}>
+				<div className={styles.sort}>
+					<Sort sortType={sortType} onChangeSort={setSortType} />
+				</div>
+				{!data.length && (isPending || isLoading) ? (
+					<Loading />
+				) : (
+					<ul className={styles.categories}>
+						{data.map((item, index) =>
+							data.length - 15 === index + 1 ? (
+								<li ref={lastNodeRef} key={item.id}>
+									<Link to={`/categories/${category}/${item.id}`}>
+										{item.name}
+									</Link>
+								</li>
+							) : (
+								<li key={item.id}>
+									<Link to={`/categories/${category}/${item.id}`}>
+										{item.name}
+									</Link>
+								</li>
+							)
+						)}
+					</ul>
+				)}
+			</div>
+		</div>
+	)
 }
