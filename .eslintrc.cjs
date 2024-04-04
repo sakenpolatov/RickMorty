6dee72cfc31f491a3c9a7c6a0f116a2ceac932e1
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-04-04 12:04:44 +0500

    feat : tsconfig

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,20 @@
 module.exports = {
-	root: true,
-	env: { browser: true, es2020: true },
-	extends: [
-		'eslint:recommended',
-		'plugin:react/recommended',
-		'plugin:react/jsx-runtime',
-		'plugin:react-hooks/recommended'
-	],
-	ignorePatterns: ['dist', '.eslintrc.cjs'],
-	parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-	settings: { react: { version: '18.2' } },
-	plugins: ['react-refresh'],
-	rules: {
-		'react/prop-types': 'off', // Отключить проверку типов props в React компонентах
-		'react/jsx-no-target-blank': 'off', // Отключить проверку на отсутствие target="_blank"
-		'react-refresh/only-export-components': [
-			'warn',
-			{ allowConstantExport: true }
-		]
-	}
-}
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+  },
+};
