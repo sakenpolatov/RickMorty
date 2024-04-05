cd1db8d852027646bffc1a80eef5bf1c31104b27
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-04-05 12:16:50 +0500

    refactor: from .jsx to .tsx

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { Suspense } from 'react'
+import { Header } from './components/Header/index'
+import { Router } from './Router/index'
+import styles from './app.module.css'
+import { AuthProvider } from './context/AuthProvider'
+import { Loading } from './components/Loading/index'
+import ErrorBoundary from './utils/ErrorBoundary/index'
+
+const App = () => {
+	return (
+		<div className={styles.app}>
+			<AuthProvider>
+				<Header />
+				<ErrorBoundary>
+					<Suspense fallback={<Loading />}>
+						<Router />
+					</Suspense>
+				</ErrorBoundary>
+			</AuthProvider>
+		</div>
+	)
+}
+
+export default App
