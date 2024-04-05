b8ee779d97dd644f756e3ef649bae80a289cd3bc
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-04-05 12:22:40 +0500

    refactor: from .jsx to .tsx

--- src/pages/Details/DetailsWrapper.tsx
+++ src/pages/Details/DetailsWrapper.tsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from 'react'
-import { Loading } from '../../components/Loading'
+import { Loading } from '../../components/Loading/index'
 
 const Details = lazy(() =>
 	import('./Details').then(module => ({
