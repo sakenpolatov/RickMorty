2aae3cce69617debc961e5e40109eff156c12db0
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-04-06 11:52:25 +0500

    feat : завел пути в отдельный файл для навигации Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,31 +2,26 @@ import { Link } from 'react-router-dom'
 import icon from '../../assets/rick.svg'
 import styles from './index.module.css'
 import { AuthStatus } from '../../context/AuthStatus/index'
-
-enum PATH_LIST {
-	characters = '/categories/character',
-	locations = '/categories/location',
-	episodes = '/categories/episode'
-}
+import { PathList } from './PathList'
 
 export function Header() {
 	return (
 		<header>
 			<div className={styles.logo}>
-				<Link to='/'>
+				<Link to={PathList.Home}>
 					<img src={icon} alt='Home' width={100} height={100} />
 				</Link>
 			</div>
 			<div className={styles.nav}>
 				<ul>
 					<li>
-						<Link to={PATH_LIST.characters}>Characters</Link>
+						<Link to={PathList.Character}>Characters</Link>
 					</li>
 					<li>
-						<Link to={PATH_LIST.locations}>Locations</Link>
+						<Link to={PathList.Location}>Locations</Link>
 					</li>
 					<li>
-						<Link to={PATH_LIST.episodes}>Episodes</Link>
+						<Link to={PathList.Episode}>Episodes</Link>
 					</li>
 				</ul>
 			</div>
