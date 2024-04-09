ebaac3599a3d934568277152c91195a62f9dbebe
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-04-09 17:00:42 +0500

    chore : добавил текст ошибки для авторизации

--- src/context/AuthProvider/index.tsx
+++ src/context/AuthProvider/index.tsx
@@ -1,43 +1,43 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useState } from 'react';
+import { ErrorMessage } from '../../constants/errorMessages';
 
 interface AuthContextType {
-	user: string | null
-	signIn: (newUser: string, callback: () => void) => void
-	signOut: (callback: () => void) => void
+  user: string | null;
+  signIn: (newUser: string, callback: () => void) => void;
+  signOut: (callback: () => void) => void;
 }
-const ERROR_MESSAGE = 'Ошибка авторизации'
 
-const AuthContext = createContext<AuthContextType | null>(null)
+const AuthContext = createContext<AuthContextType | null>(null);
 
 export const useAuth = (): AuthContextType => {
-	const context = useContext(AuthContext)
-	if (!context) {
-		throw new Error(ERROR_MESSAGE)
-	}
-	return context
-}
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error(ErrorMessage.ErrorAuth);
+  }
+  return context;
+};
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
-	const [user, setUser] = useState<string | null>(() => {
-		return localStorage.getItem('user')
-	})
+  const [user, setUser] = useState<string | null>(() => {
+    return localStorage.getItem('user');
+  });
 
-	const signIn = (newUser: string, callback: () => void) => {
-		setUser(newUser)
-		localStorage.setItem('user', newUser)
-		callback()
-	}
-	const signOut = (callback: () => void) => {
-		setUser(null)
-		localStorage.removeItem('user')
-		callback()
-	}
+  const signIn = (newUser: string, callback: () => void) => {
+    setUser(newUser);
+    localStorage.setItem('user', newUser);
+    callback();
+  };
+  const signOut = (callback: () => void) => {
+    setUser(null);
+    localStorage.removeItem('user');
+    callback();
+  };
 
-	const value: AuthContextType = {
-		user,
-		signIn,
-		signOut
-	}
+  const value: AuthContextType = {
+    user,
+    signIn,
+    signOut,
+  };
 
-	return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
