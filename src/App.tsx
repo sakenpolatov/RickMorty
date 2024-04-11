6d0c24fa41e41042432e2c95851cf4470c436de4
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-04-11 14:46:53 +0500

    feat : mantine provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import { Suspense } from 'react'
-import { Header } from './components/Header/index'
-import { Router } from './Router/index'
-import styles from './app.module.css'
-import { AuthProvider } from './context/AuthProvider'
-import { Loading } from './components/Loading/index'
-import ErrorBoundary from './components/ErrorBoundary/index'
+import { Suspense } from 'react';
+import { Header } from './components/Header/index';
+import { Router } from './Router/index';
+import styles from './app.module.css';
+import { AuthProvider } from './context/AuthProvider';
+import { Loading } from './components/Loading/index';
+import ErrorBoundary from './components/ErrorBoundary/index';
+import { MantineProvider, createTheme } from '@mantine/core';
+import '@mantine/core/styles.css';
+const theme = createTheme({
+  fontFamily: 'Montserrat, sans-serif',
+  defaultRadius: 'md',
+});
 
 const App = () => {
-	return (
-		<div className={styles.app}>
-			<AuthProvider>
-				<Header />
-				<ErrorBoundary>
-					<Suspense fallback={<Loading />}>
-						<Router />
-					</Suspense>
-				</ErrorBoundary>
-			</AuthProvider>
-		</div>
-	)
-}
+  return (
+    <MantineProvider theme={theme}>
+      <div className={styles.app}>
+        <AuthProvider>
+          <Header />
+          <ErrorBoundary>
+            <Suspense fallback={<Loading />}>
+              <Router />
+            </Suspense>
+          </ErrorBoundary>
+        </AuthProvider>
+      </div>
+    </MantineProvider>
+  );
+};
 
-export default App
+export default App;
